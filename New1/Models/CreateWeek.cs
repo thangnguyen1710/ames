8faed32f074d4603eaacf3fff0880b0935ee3a1d
thangn <thangnguyen17102000@gmail.com>
2023-07-14 09:48:20 +0700

    crud

---
+++ New1/Models/CreateWeek.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace New1.Models
+{
+    public class CreateWeek
+    {
+        public int CityId { get; set; }
+        public  int Week { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+     
+    }
+}
---
+++ New1/Models/DeleteWeek.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace New1.Models
+{
+    public class DeleteWeek
+    {
+        public int Id { get; set; }
+        public int CityId { get; set; }
+    }
+}
